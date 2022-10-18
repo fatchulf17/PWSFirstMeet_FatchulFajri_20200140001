6cfd2d26971f92f7bc438d98e11ff8ec4d3ad0b4
fatchulf17 <96380224+fatchulf17@users.noreply.github.com>
2022-10-18 21:37:13 +0700

    firstmeet PWS

---
+++ src/test/java/iniperpus/learnmigratedb/LearnmigratedbApplicationTests.java
@@ -0,0 +1,13 @@
+package iniperpus.learnmigratedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnmigratedbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
